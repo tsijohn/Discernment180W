1692a0e49f3467f22a22578ffccd144db8c5a2f8
John Kim <utjohnkkim@gmail.com>
2024-08-17 10:00:39 -0500

    Initial Commit

---
+++ Discernment180W/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Discernment180W
+//
+//  Created by John Kim on 8/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Discernment180W/Discernment180WApp.swift
@@ -0,0 +1,17 @@
+//
+//  Discernment180WApp.swift
+//  Discernment180W
+//
+//  Created by John Kim on 8/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Discernment180WApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
